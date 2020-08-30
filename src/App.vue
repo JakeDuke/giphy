11d5c7c678720710e0ce06c4b80d2565f5b1b671
JakeDuke <36011510+JakeDuke@users.noreply.github.com>
2020-08-30 20:03:21 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,26 @@
 </template>
 
 <style lang="scss">
+html, body {
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 8px 12px;
+  height: 100%;
+  background-image: linear-gradient(-120deg, rgb(194, 3, 194), rgb(51, 1, 51));
 }
 
 #nav {
-  padding: 30px;
+  // padding: 30px;
+  // position: absolute;
+  top: 0;
+  width: 100%;
 
   a {
     font-weight: bold;
---
+++ src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrap">
+    <div class="register">
+      <b-form-input v-model="email" placeholder="Enter your email"></b-form-input>
+      <b-form-input v-model="password" placeholder="Enter your password"></b-form-input>
+    </div>
+    <b-button pill variant="success">Register</b-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  email = ''
+  password = ''
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.wrap {
+  width: 100%;
+}
+
+input {
+  margin: 20px 0;
+}
+
+.btn {
+  margin-top: 30px;
+  padding: 10px 20px;
+} 
+</style>

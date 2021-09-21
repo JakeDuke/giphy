b0d6b2472d8a46ae68dd361a0e1221286445bebd
Jake Dukin <jake.dukin@bohemian.studio>
2021-09-21 18:08:17 +0200

    pagination added

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,12 @@ body {
   height: 100%;
 }
 
-$colour: rgb(10, 1, 2);
-$grey: desaturate($colour, 80%);
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: whitesmoke;
-  padding: 8px 12px;
+  color: black;
   height: 100%;
-}
-
-#nav {
-  top: 0;
-  width: 100%;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="container">
-    <!-- <b-spinner v-if="loading" /> -->
-
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/8/82/Giphy-logo.svg"
       alt=""
@@ -10,14 +8,27 @@
 
     <b-form-input
       v-model="searchQuery"
-      placeholder="Giphy name"
+      placeholder="Type giphy name here"
       debounce="300"
     ></b-form-input>
 
-    <div v-if="giphys" class="results">
-      <div v-for="giphy in giphys" :key="giphy.id" class="item">
-        <img :src="giphy.images.original.url" alt="giphy" />
+    <div v-if="error">
+      <h3>{{ error }}</h3>
+    </div>
+
+    <div v-else-if="giphys" class="wrap">
+      <div class="results">
+        <div v-for="giphy in giphys" :key="giphy.id" class="item">
+          <img :src="giphy.images.original.url" alt="giphy" />
+        </div>
       </div>
+
+      <b-pagination
+        v-if="totalCount > perPage"
+        v-model="currentPage"
+        :total-rows="totalCount"
+        :per-page="perPage"
+      ></b-pagination>
     </div>
   </div>
 </template>
@@ -31,34 +42,68 @@ import axios from "axios";
 })
 export default class Home extends Vue {
   searchQuery = "";
-
+  perPage = 21;
+  currentPage = 1;
+  totalCount = 0;
   giphys = [];
+  error = "";
+
+  get offset() {
+    return this.perPage * (this.currentPage - 1);
+  }
 
   get url() {
-    return `https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=${this.searchQuery}`;
+    return `https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=${this.searchQuery}&offset=${this.offset}&limit=${this.perPage}`;
   }
 
   async getGiphys() {
-    const { data } = await axios.get(this.url);
-    this.giphys = data.data;
+    this.error = "";
+    this.giphys = [];
+    try {
+      const { data } = await axios.get(this.url);
+      this.totalCount = data.pagination.total_count;
+      this.giphys = data.data;
+    } catch (e) {
+      this.error = "Nothing was found";
+    }
   }
 
   @Watch("searchQuery")
-  onSearchQueryhanged() {
+  onSearchQuerychanged() {
     this.getGiphys();
+  }
+
+  @Watch("currentPage")
+  onCurrentPagechanged() {
+    this.getGiphys();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .results {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 20px 5px;
+  margin: 3rem 0;
+  width: 100%;
+}
+
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 }
 
 .item {
